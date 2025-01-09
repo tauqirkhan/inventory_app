ac297390d21eb66dacd4bf371272a976b6295826
Tauqir <tauqirkhann.11@gmail.com>
2025-01-10 02:18:29 +0530

    feat: link quotes and author via "/:id/quotes"

--- controllers/getAuthorQuotes.js
+++ controllers/getAuthorQuotes.js
@@ -2,18 +2,20 @@ const db = require("../db/queries");
 const CustomNotFoundError = require("../errors/CustomNotFoundError");
 const asyncHandler = require("express-async-handler");
 
-const getIndex = asyncHandler(async (req, res) => {
-  const authorsArray = await db.getAllAuthorsNameArray();
-  const firstAuthorQuotesArray = await db.getAllQuotesArrayByAuthorId(1);
+const getAuthorQuotes = asyncHandler(async (req, res) => {
+  const { author_id } = req.params;
 
-  if (!firstAuthorQuotesArray)
+  const authorsArray = await db.getAllAuthorsNameArray();
+  const quotesArray = await db.getAllQuotesArrayByAuthorId(author_id);
+
+  if (!quotesArray)
     throw new CustomNotFoundError("Not correct! Quotes not found");
   if (!authorsArray) throw new CustomNotFoundError("Cannot found authors");
 
   res.render("app", {
     authorsArray: authorsArray,
-    quotesArray: firstAuthorQuotesArray,
+    quotesArray: quotesArray,
   });
 });
 
-module.exports = getIndex;
+module.exports = getAuthorQuotes;
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const getIndex = require("../controllers/getIndex");
+const getAuthorQuotes = require("../controllers/getAuthorQuotes");
 
 const indexRouter = Router();
 
 indexRouter.get("/", getIndex);
+indexRouter.get("/:author_id/quotes", getAuthorQuotes);
 
 module.exports = indexRouter;
