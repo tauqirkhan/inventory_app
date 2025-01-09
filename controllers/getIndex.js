b02eafceebc5b25f4b36d92b7d5fd5e03ae33376
Tauqir <tauqirkhann.11@gmail.com>
2025-01-09 23:50:32 +0530

    feat: add quote in index "/" route of first name

--- controllers/getIndex.js
+++ controllers/getIndex.js
@@ -2,7 +2,11 @@ const db = require("../db/queries");
 
 const getIndex = async (req, res) => {
   const authorsArray = await db.getAllAuthorsNameArray();
-  res.render("index", { authorsArray: authorsArray });
+  const firstAuthorQuotesArray = await db.getAllQuotesArrayByAuthorId(1);
+  res.render("index", {
+    authorsArray: authorsArray,
+    firstAuthorQuotesArray: firstAuthorQuotesArray,
+  });
 };
 
 module.exports = getIndex;
