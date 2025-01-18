50319276fb78a57cbede690cd70e5792a118e6b8
Tauqir <tauqirkhann.11@gmail.com>
2025-01-18 16:43:19 +0530

    feat: add adminAuthentication to update authorName

--- controllers/updateAuthorNamePost.js
+++ controllers/updateAuthorNamePost.js
@@ -1,12 +1,20 @@
 const db = require("../db/queries");
+const getAuthorQuotes = require("./getAuthorQuotes");
+require("dotenv").config();
 
 const updateAuthorNamePost = async (req, res) => {
   const { author_id } = req.params;
-  const { newAuthorName } = req.body;
+  const { newAuthorName, adminPassword } = req.body;
 
-  await db.updateAuthorNamePost(newAuthorName, author_id);
+  if (process.env.ADMINPASSWORD === String(adminPassword)) {
+    await db.updateAuthorNamePost(newAuthorName, author_id);
 
-  res.redirect(`/${author_id}/quotes`);
+    res.redirect(`/${author_id}/quotes`);
+    return;
+  }
+
+  res.locals.errors = [{ msg: "Incorrect admin password" }];
+  await getAuthorQuotes(req, res);
 };
 
 module.exports = updateAuthorNamePost;
