996b3b1fcced396931069576646ff698feb590e8
Tauqir <tauqirkhann.11@gmail.com>
2025-01-21 06:32:06 +0530

    feat: add error page and customNotFound error

--- controllers/getAuthorQuotes.js
+++ controllers/getAuthorQuotes.js
@@ -1,12 +1,19 @@
+const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
 const checkQueryAndAddError = require("./utils/checkQueryAndAddError");
+const CustomNotFoundError = require("../errors/CustomNotFoundError");
 
-const getAuthorQuotes = async (req, res) => {
+const getAuthorQuotes = asyncHandler(async (req, res) => {
   const { error_type } = req.query;
   const { author_id } = req.params;
   const authorsArray = await db.getAllAuthorsNameArray();
   const quotesArray = await db.getAllQuotesArrayByAuthorId(author_id);
   const authorName = await db.getAuthorNameById(author_id);
+
+  //Only found authorName is string
+  if (typeof authorName !== "string") {
+    throw new CustomNotFoundError("Author not found");
+  }
 
   //To show error manually for password mismatch through locals
   checkQueryAndAddError(error_type, res);
@@ -17,6 +24,6 @@ const getAuthorQuotes = async (req, res) => {
     authorName: authorName,
     author_id: author_id,
   });
-};
+});
 
 module.exports = getAuthorQuotes;
